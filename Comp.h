2c11f87ddb6266f41bcb4d659890dc9fabf6fec5
Hugochagas7878 <135867774+Hugochagas7878@users.noreply.github.com>
2023-11-06 19:36:40 -0300

    Update Comp.h

--- Comp.h
+++ Comp.h
@@ -4,6 +4,10 @@
         Laura Soléria Lobo Vitorino Maciel - 555523
 */
 
+
+//  NÃO NECESSÁRIA PARA A PRIMEIRA PARTE DO PROJETO
+
+
 #ifndef COMPUTADOR_H
 #define COMPUTADOR_H
 
@@ -13,14 +17,19 @@
 #include "Queue.h"
 #include "Priority_queue.h"
 #include "Processos.h"
+
 using namespace std;
 
 class Computador {
 private:
     vector< Process* > Processos;
+    vector< Evento   > Eventos;    
     Queue<int > CPU;
     Queue<int > Disco1;
     Queue<int > Disco2;
+    //PriorityQueue<int > CPUp;
+    //PriorityQueue<int > Disco1p;
+    //PriorityQueue<int > Disco2p;
 
 public:
 
@@ -28,17 +37,6 @@ public:
 
     void addProcess( Process* process) {
         Processos.push_back(process);
-        CPU.push(process ->TempoCPU);
-        random_device rd; // Seed para o gerador de números aleatórios
-        mt19937 gen(rd()); // Mersenne Twister engine
-        uniform_int_distribution<> dis(0, 1); // Distribuição uniforme para escolher entre os vetores 0 e 
-        int escolha = dis(gen); // Escolhe aleatoriamente entre 0 e 1
-
-        if (escolha == 0) {
-            Disco1.push(process ->TempoDisco);
-        } else {
-            Disco2.push(process ->TempoDisco);
-        }
     }
 
 
@@ -50,11 +48,26 @@ public:
         if(!Processos.empty())
         for (auto processo : Processos) {
             if (processo->Instante == currentTime) {
-                // Processo chegou: adicione à CPU (ou à fila relevante, dependendo do seu algoritmo)
-                cout << "Tempo: " << currentTime << " - Processo " << processo->id << " chegou na CPU" << endl;
+                //passo para verificar se é o primeiro processo da cpu
+                    //if()
+                    CPU.push(processo ->TempoCPU);
+                
             }
         }
+        //refazer a logica
+        //implementar a base para a priority_queue
+        /*
+        random_device rd; // Seed para o gerador de números aleatórios
+        mt19937 gen(rd()); // Mersenne Twister engine
+        uniform_int_distribution<> dis(0, 1); // Distribuição uniforme para escolher entre os vetores 0 e 
+        int escolha = dis(gen); // Escolhe aleatoriamente entre 0 e 1
 
+        if (escolha == 0) {
+            Disco1.push(process ->TempoDisco);
+        } else {
+            Disco2.push(process ->TempoDisco);
+        }
+        */
         
     }
     
