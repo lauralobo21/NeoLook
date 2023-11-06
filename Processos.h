4031fafe2338749466414b54783fd2094b57a1de
Hugochagas7878 <135867774+Hugochagas7878@users.noreply.github.com>
2023-11-06 11:26:08 -0300

    Update Processos.h

---
+++ Processos.h
@@ -0,0 +1,17 @@
+#ifndef PROCESSOS_H
+#define PROCESSOS_H
+
+struct Process 
+{
+    int id;             // ID do processo
+    int Instante;        // Tempo de início da execução do processo
+    int TempoCPU;       // Tempo de CPU necessário para o processo
+    int TempoDisco;     // Tempo de disco necessário para o processo
+    int TempoRede;      // Tempo de rede necessário para o processo
+    int ComputadorId;   // ID do computador onde o processo está sendo executado
+    int TempoDeEsp;
+    int TempoDeExe;
+    bool terminado;
+};
+
+#endif // PROCESSOS_H
