3741036b03a691bf13169fcea5130bbb120c7308
Hugochagas7878 <135867774+Hugochagas7878@users.noreply.github.com>
2023-11-06 19:35:21 -0300

    Update Eventos.h

--- Eventos.h
+++ Eventos.h
@@ -7,7 +7,18 @@ struct Evento
 
     int instanteDeTempo;   // Tempo do evento 
     string acontecimento;  // Descrição do evento
+    // posso fazer um codigo de registro, para facilitar a leitura pelo codigo
+    // assim, ao inves de ler o acontecimente, ele iria ler o codigo do acontecimento
+    int indetifica;        // codigo de registro do acontecimento
 
 };
-
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //modelo de impressao
+    //cout << "Tempo: " << currentTime << " - Processo " << processo->id << " chegou na CPU" << endl;
 #endif
